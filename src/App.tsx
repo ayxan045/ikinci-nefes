5dac1c3c49d2e955053c72173823724a8da01579
Ayxan <ayxan.axmed26@gmail.com>
2026-05-05 17:03:02 +0400

    vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import HaqqimızdaPage from './pages/HaqqimızdaPage';
-import XidmetlerPage from './pages/XidmetlerPage';
-import PsixoloqlarPage from './pages/PsixoloqlarPage';
-import TestlerPage from './pages/TestlerPage';
-import BloqPage from './pages/BloqPage';
-import ElaqePage from './pages/ElaqePage';
-import DepressiyaMualicesiPage from './pages/DepressiyaMualicesiPage';
-import NotFoundPage from './pages/NotFoundPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import HaqqimızdaPage from "./pages/HaqqimızdaPage";
+import XidmetlerPage from "./pages/XidmetlerPage";
+import PsixoloqlarPage from "./pages/PsixoloqlarPage";
+import TestlerPage from "./pages/TestlerPage";
+import BloqPage from "./pages/BloqPage";
+import ElaqePage from "./pages/ElaqePage";
+import DepressiyaMualicesiPage from "./pages/DepressiyaMualicesiPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"                                    element={<HomePage />} />
-        <Route path="/haqqimizda"                          element={<HaqqimızdaPage />} />
-        <Route path="/xidmetler"                           element={<XidmetlerPage />} />
-        <Route path="/xidmetler/depressiya-mualicesi"      element={<DepressiyaMualicesiPage />} />
-        {/* TODO: add individual service detail pages */}
-        <Route path="/psixoloqlar"                         element={<PsixoloqlarPage />} />
-        {/* TODO: /psixoloqlar/:id  — PsixoloqDetailPage */}
-        <Route path="/testler"                             element={<TestlerPage />} />
-        {/* TODO: /testler/:slug    — TestDetailPage */}
-        <Route path="/bloq"                                element={<BloqPage />} />
-        {/* TODO: /bloq/:slug       — BlogPostDetailPage */}
-        <Route path="/elaqe"                               element={<ElaqePage />} />
-        <Route path="*"                                    element={<NotFoundPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/haqqimizda" element={<HaqqimızdaPage />} />
+        <Route path="/xidmetler" element={<XidmetlerPage />} />
+        <Route
+          path="/xidmetler/depressiya-mualicesi"
+          element={<DepressiyaMualicesiPage />}
+        />
+        <Route path="/psixoloqlar" element={<PsixoloqlarPage />} />
+        <Route path="/testler" element={<TestlerPage />} />
+        <Route path="/bloq" element={<BloqPage />} />
+        <Route path="/elaqe" element={<ElaqePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
